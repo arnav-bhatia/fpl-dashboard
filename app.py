133c42d0bb33a0003bcefe82cf90088569cca781
Arnav Bhatia <arnavbhatia274@gmail.com>
2025-08-10 06:11:00 +0530

    fix(app): removed menu items from page config.

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title='FPL Analyzer',
     layout='wide',
-    initial_sidebar_state='auto',
-    menu_items={
-        'Github Repo': 'https://github.com/arnav-bhatia/fpl-dashboard',
-        'About': 'A tool to help you choose the best players for your FPL team'
-    }
+    initial_sidebar_state='auto'
 )
 
 st.title('FPL Analyzer')
